bc8e17cbfc35f8d5b2d249c1a5a391be0abfa2a2
Nolan <nolanjupvp@gmail.com>
2025-04-04 14:26:53 +0200

    ajout routine

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,14 +6,23 @@
 /*   By: njung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:29:39 by njung             #+#    #+#             */
-/*   Updated: 2025/03/27 10:35:04 by njung            ###   ########.fr       */
+/*   Updated: 2025/04/04 14:26:31 by njung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
 #include "philosophers.h"
 
-void *philosopher_routine(void *args)
+void *philosopher_routine(void *arg)
 {
-    
-}*/
+    t_philo *philo = (t_philo *)arg;
+
+    while (!simulation_is_over(philo))
+    {
+        take_forks(philo);
+        eat(philo);
+        put_down_forks(philo);
+        sleep_philosopher(philo);
+        think(philo);
+    }
+    return NULL;
+}
